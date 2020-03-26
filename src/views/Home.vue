4b720c712ed21efb8d4ccbae848a0919e33ed917
herlambangK <herlambangk25@gmail.com>
2020-03-26 23:42:07 +0700

    upload project Vue

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomenShayna />
+    <InstaShayna />
+    <PatnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+//melakukan exspoert dan jangn lupa menuliskan code <nama />
+// import carousel from "vue-owl-carousel"; -> dipindah ke heroshayna
+import HeaderShayna from "@/components/HeaderShayna.vue"; //setelah meinport dan exsport dapat langung
+import HeroShayna from "@/components/HeroShayna.vue";
+import WomenShayna from "@/components/WomenShayna.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PatnerShayna from "@/components/PatnerShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenShayna,
+    InstaShayna,
+    PatnerShayna,
+    FooterShayna
+  }
+};
+</script>
